73c0f77a2b4679e1965ebc7d9a4f8f08dc6fe19f
Fayadh <nugrahafayadh@gmail.com>
2025-11-10 21:33:06 +0700

    Deklarasi Fungsi-Fungsi

--- Kasir.cpp
+++ Kasir.cpp
@@ -2,6 +2,13 @@
 #include<windows.h>
 using namespace std;
 
+
+void header();
+void menuUtama();
+void tambahBarang(Barang barang[], int &jumlah);
+void tampilBarang(Barang barang[], int jumlah);
+void cetakStruk(Barang barang[], int jumlah);
+
 int main(){
 	
 }
