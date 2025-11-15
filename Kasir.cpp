f25fde503e80fa4d54e6b28357804be0e3af9ea8
Jonathan <jonathanjohnx12@gmail.com>
2025-11-15 22:09:03 +0700

    Update Kasir.cpp
    Add function to display items in the inventory.

--- Kasir.cpp
+++ Kasir.cpp
@@ -9,6 +9,21 @@ void tambahBarang(Barang barang[], int &jumlah);
 void tampilBarang(Barang barang[], int jumlah);
 void cetakStruk(Barang barang[], int jumlah);
 
+void tampilBarang(Barang barang[], int jumlah) {
+    if (jumlah == 0) {
+        cout << "\nBelum ada barang yang dimasukkan.\n";
+        return;
+    }
+
+    cout << "\n---------------------------------------------\n";
+    cout << "No | Nama Barang                 | Harga (Rp)\n";
+    cout << "---------------------------------------------\n";
+    for (int i = 0; i < jumlah; i++) {
+        cout << (i + 1) << "  | " << barang[i].nama << " | Rp " << barang[i].harga << endl;
+    }
+    cout << "---------------------------------------------\n";
+}
+
 int main(){
 	
 }
