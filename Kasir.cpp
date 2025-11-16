37452148a49efead90982f61a79679bf0c62e4c4
Aripe-netizen <ahmadarifkurniawan44@gmail.com>
2025-11-16 11:33:35 +0700

    nambah fungsi tambahBarang
    Implement function to add new items to inventory

--- Kasir.cpp
+++ Kasir.cpp
@@ -9,6 +9,25 @@ void tambahBarang(Barang barang[], int &jumlah);
 void tampilBarang(Barang barang[], int jumlah);
 void cetakStruk(Barang barang[], int jumlah);
 
+void tambahBarang(Barang barang[], int &jumlah) {
+    int n;
+    cout << "\nBerapa banyak barang yang ingin ditambahkan? ";
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++) {
+        cout << "\nBarang ke-" << (jumlah + 1) << endl;
+        cout << "Nama Barang : ";
+        getline(cin, barang[jumlah].nama);
+        cout << "Harga (Rp)  : ";
+        cin >> barang[jumlah].harga;
+        cin.ignore();
+        jumlah++;
+    }
+
+    cout << "\nBarang berhasil ditambahkan!\n";
+}
+
 void tampilBarang(Barang barang[], int jumlah) {
     if (jumlah == 0) {
         cout << "\nBelum ada barang yang dimasukkan.\n";
