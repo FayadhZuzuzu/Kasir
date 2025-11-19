9c84fad744dfc36d8faceef0d50b7974043a44fb
Bibiii <fhs.1630v@gmail.com>
2025-11-19 23:01:53 +0700

    Update Kasir.cpp

--- Kasir.cpp
+++ Kasir.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include<windows.h>
+#include <windows.h>
 using namespace std;
 
 struct Barang {
@@ -8,20 +8,24 @@ struct Barang {
 };
 
 
-
 void header();
 void menuUtama();
 void tambahBarang(Barang barang[], int &jumlah);
 void tampilBarang(Barang barang[], int jumlah);
 void cetakStruk(Barang barang[], int jumlah);
+void Animasipembuka();
+void cetakPelan(string text);
+void typeText(string text, int speed = 50);
 
 int main() {
     Barang barang[100];
     int jumlah = 0;
     int pilihan;
 
+    Animasipembuka();
+
     do {
-        system("cls"); 
+        system("cls");
         header();
         menuUtama();
         cout << "Pilih menu: ";
@@ -53,7 +57,42 @@ int main() {
 
     return 0;
 }
-void header(){
+
+
+
+
+void typeText(string text, int speed) {
+    for (char c : text) {
+        cout << c;
+        Sleep(speed);
+    }
+}
+
+void Animasipembuka() {
+    system("cls");
+    typeText("Loading Program Kasir...\n", 40);
+    Sleep(300);
+
+    for (int i = 0; i <= 40; i++) {
+        system("cls");
+        cout << "Memulai Program...\n\n";
+        cout << "[";
+
+        for (int j = 0; j < i; j++) cout << "=";
+        for (int j = i; j < 40; j++) cout << " ";
+
+        cout << "] " << i * 2.5 << "%";
+        Sleep(60);
+    }
+
+    system("cls");
+    typeText("Program Berhasil Dibuka!\n\n", 35);
+}
+
+
+
+
+void header() {
     cout << "=============================================\n";
     cout << "  >>>>>     PROGRAM KASIR MINI       <<<<<   \n";
     cout << "=============================================\n\n";
@@ -65,6 +104,10 @@ void menuUtama() {
     cout << "3. Cetak Struk Pembelian\n";
     cout << "4. Keluar\n\n";
 }
+
+
+
+
 void tambahBarang(Barang barang[], int &jumlah) {
     int n;
     cout << "\nBerapa banyak barang yang ingin ditambahkan? ";
@@ -84,6 +127,9 @@ void tambahBarang(Barang barang[], int &jumlah) {
     cout << "\nBarang berhasil ditambahkan!\n";
 }
 
+
+
+
 void tampilBarang(Barang barang[], int jumlah) {
     if (jumlah == 0) {
         cout << "\nBelum ada barang yang dimasukkan.\n";
@@ -93,8 +139,55 @@ void tampilBarang(Barang barang[], int jumlah) {
     cout << "\n---------------------------------------------\n";
     cout << "No | Nama Barang                 | Harga (Rp)\n";
     cout << "---------------------------------------------\n";
+
     for (int i = 0; i < jumlah; i++) {
-        cout << (i + 1) << "  | " << barang[i].nama << " | Rp " << barang[i].harga << endl;
+        cout << (i + 1) << "  | " << barang[i].nama 
+             << " | Rp " << barang[i].harga << endl;
     }
+
     cout << "---------------------------------------------\n";
 }
+
+
+
+
+void cetakPelan(string text) {
+    cout << text << endl;
+    Sleep(300);
+}
+
+void cetakStruk(Barang barang[], int jumlah) {
+
+    if (jumlah == 0) {
+        cout << "\nBelum ada barang yang dimasukkan.\n";
+        return;
+    }
+
+    double total = 0;
+    for (int i = 0; i < jumlah; i++) {
+        total += barang[i].harga;
+    }
+
+    system("cls");
+    cout << "Mencetak struk...\n\n";
+    Sleep(400);
+
+    cetakPelan("====================================");
+    cetakPelan("            STRUK BELANJA           ");
+    cetakPelan("====================================");
+    cetakPelan("Daftar Barang:");
+
+   
+    for (int i = 0; i < jumlah; i++) {
+        string baris = "- " + barang[i].nama + " : Rp " + to_string((int)barang[i].harga);
+        cetakPelan(baris);
+    }
+
+    cetakPelan("------------------------------------");
+    cetakPelan("Jumlah Item : " + to_string(jumlah));
+    cetakPelan("Total       : Rp " + to_string((int)total));
+    cetakPelan("------------------------------------");
+    cetakPelan("Terima kasih sudah berbelanja!");
+    cetakPelan("====================================");
+}
+
